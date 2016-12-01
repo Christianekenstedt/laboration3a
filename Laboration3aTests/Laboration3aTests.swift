3bcfbf0f44eb6c092bbfdf3e8d9312e9b9f5cc13
Christian <asd>
2016-12-01 18:22:11 +0100

    Initial Commit

---
+++ Laboration3aTests/Laboration3aTests.swift
@@ -0,0 +1,36 @@
+//
+//  Laboration3aTests.swift
+//  Laboration3aTests
+//
+//  Created by Christian Ekenstedt on 2016-12-01.
+//  Copyright © 2016 Christian Ekenstedt. All rights reserved.
+//
+
+import XCTest
+@testable import Laboration3a
+
+class Laboration3aTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
